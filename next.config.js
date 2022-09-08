42e07d37fe8b686933894102db93e3f2c5b3bdbb
hants <hantsawilliams@gmail.com>
2022-09-08 12:45:54 -0400

    turned off

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // next.config.js
 // if deploy with vercel, will need to deploy vercel.json file with this: https://vercel.com/guides/how-to-enable-cors#enabling-cors-using-vercel.json
 
-module.exports = {
-  };
+// module.exports = {
+//   };
