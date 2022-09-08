189d098398c4e46c06a94660f1b508b969918f77
hants <hantsawilliams@gmail.com>
2022-09-08 10:42:01 -0400

    name change

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 import { Github, Twitter, Mail } from '../components/icons';
 
-export const MY_APP = 'Digital Clone';
+export const MY_APP = 'SBU-SHP Digital Clone';
 export const MY_NAME = 'Hants Williams';
 export const TWITTER_USER_NAME = 'hants_williams';
 export const PLATFORM_URL = 'https://www.appliedhealthinformatics.com/';
